68e57a1aac115d24c62379486cc947838af62357
Alex Lee <x.lee@mail.utoronto.ca>
2017-05-31 09:32:09 -0700

    Initial commit

---
+++ Threelow/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Threelow
+//
+//  Created by Alex Lee on 2017-05-31.
+//  Copyright © 2017 Alex Lee. All rights reserved.
+//
+
+//USE THESE SYMBOLS --> ⚀ ⚁ ⚂ ⚃ ⚄ ⚅
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
